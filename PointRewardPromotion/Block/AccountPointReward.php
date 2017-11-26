0b35136da7d97fb229273c9c6a5b886bb7b4ff43
Chau Nguyen <dangchau1991@gmail.com>
2017-11-26 16:10:22 +0200

    Changed: Update Readme to include screenshot Changed: point-reward-link in top.links moved to header.links Fixed: Remove duplicated wishlist script

--- PointRewardPromotion/Block/AccountPointReward.php
+++ PointRewardPromotion/Block/AccountPointReward.php
@@ -15,14 +15,22 @@ class AccountPointReward extends \Magento\Framework\View\Element\Html\Link
     protected $_template = 'Magento_PointRewardPromotion::point_reward.phtml';
 
     /**
+     * @var \Magento\Framework\App\Http\Context
+     */
+    protected $httpContext;
+
+    /**
      * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Framework\App\Http\Context $httpContext
      * @param array $data
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\App\Http\Context $httpContext,
         array $data = []
     ) {
         parent::__construct($context, $data);
+        $this->httpContext = $httpContext;
     }
 
     /**
@@ -39,5 +47,15 @@ class AccountPointReward extends \Magento\Framework\View\Element\Html\Link
     public function getLabel()
     {
         return __('Reward');
+    }
+
+    /**
+     * Checking customer login status
+     *
+     * @return bool
+     */
+    public function customerLoggedIn()
+    {
+        return (bool)$this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_AUTH);
     }
 }
--- Readme.md
+++ Readme.md
@@ -7,3 +7,17 @@ The module will add a new reward system, every products will have a point. A cus
 
 Clone the repo to `app/code/Magento`    
 Run `php bin/magento setup:upgrade` and `php bin/magento pointreward:init`
+
+# Screenshot
+
+1. Customer discount level and collected points        
+![customer total point](/screenshots/s0.png?raw=true)    
+
+2. Product reward attributes    
+![product reward attributes](/screenshots/s1.png?raw=true)    
+
+3. MiniCart layout     
+![minicart layout](/screenshots/s2.png?raw=true)    
+
+4. Summary layout (with 5% discount)    
+![summary layout](/screenshots/s3.png?raw=true)    
